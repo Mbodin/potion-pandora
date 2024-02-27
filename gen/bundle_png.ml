e36ada6a590ee0935ee85ea2cd26f7b54e37eceb
Martin Bodin <martin.bodin@inria.fr>
2024-02-27 12:02:37 +0100

    Ajout d'images brutes.

--- gen/bundle_png.ml
+++ gen/bundle_png.ml
@@ -3,29 +3,31 @@
 
 let image_folder = "images"
 
-let regexp_name = Str.regexp {|\([^.]*\).png|}
-let regexp_forbidden_names = Str.regexp {|\(\)|}
+let regexp_name = Str.regexp {|^\([^.]*\).png$|}
+let regexp_forbidden_names = Str.regexp {|^\(\)$|}
 
 let all_images =
   let all_files = Array.to_list (Sys.readdir image_folder) in
   let all_filenames =
     List.filter_map (fun file ->
       if Str.string_match regexp_name file 0 then (
-        let file = Str.matched_group 1 file in
+        let name = Str.matched_group 1 file in
         if Str.string_match regexp_forbidden_names file 0 then
-          Printf.eprintf "Warning: unexpected file name %s.png." file ;
-        Some file
+          Printf.eprintf "Warning: unexpected file name %s.\n" file ;
+        Some (name, image_folder ^ "/" ^ file)
       ) else (
-        Printf.eprintf "Warning: file %s not a png file." file ;
+        Printf.eprintf "Warning: file %s not a png file.\n" file ;
         None
       )) all_files in
-  List.map (fun file ->
-    let file = Printf.sprintf "%s/%s.png" image_folder file in
+  List.map (fun (name, file) ->
     let channel = open_in_bin file in
     let len = in_channel_length channel in
     let content = really_input_string channel len in
     let chunk = ImageUtil.chunk_reader_of_string content in
-    ImageLib.PNG.parsefile chunk) all_filenames
+    (name, ImageLib.PNG.parsefile chunk)) all_filenames
+
+let max_width =
+  List.fold_left (fun w (_name, image) -> max w image.Image.width) 1 all_images
 
 let () =
   print_endline {|[@@@warning "-32"]|} ;
