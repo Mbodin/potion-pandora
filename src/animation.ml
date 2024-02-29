4cace366592bbf5a2a90dbdbc82faf82f6350d3e
Martin Bodin <martin.bodin@inria.fr>
2024-02-29 17:02:53 +0100

    Réflexion sur du code pour gérer l'animation.

--- src/animation.ml
+++ src/animation.ml
@@ -7,3 +7,126 @@ type subimage = {
 
 let make_subimage width height position = { width ; height ; position }
 
+type event =
+  | MoveLeft
+  | MoveRight
+  | LookDown
+  | LookUp
+  | LookBehind
+  | Touch
+  | Wind
+  | RandomFlicker
+  | RandomFrequent
+  | RandomNormal
+  | RandomRare
+  | Tau
+
+(* A module for events for the Map module. *)
+module Event = struct
+  type t = event
+  let compare = compare
+end
+
+module EMap = Map.Make (Event)
+module ESet = Set.Make (Event)
+
+(* The representation of an object state. *)
+type state = int
+
+(* A type to count time, as a number of cycles.
+  The type is abstracted away to avoid mixing with the state. *)
+module Time : sig
+  type t
+  val zero : t
+  val incr : t -> t
+  val of_int : int -> t
+  val compare : t -> t -> int
+  val (<) : t -> t -> bool
+
+  (* This converts a time in seconds into a time in number of frames. *)
+  val of_seconds : float -> t
+
+end = struct
+  type t = int
+  let zero = 0
+  let incr t =
+    if t = max_int then max_int
+    else t + 1
+  let of_int t = t
+  let compare = compare
+  let (<) = (<)
+
+  (* Number of frame per seconds. *)
+  let frame_per_seconds = 60
+
+  let of_seconds s =
+    Float.to_int (Float.round (s *. Float.of_int frame_per_seconds))
+
+end
+type time = Time.t
+
+(* The internal automaton of an object.
+  Each state is an array index.
+  It then stores the associated image, as well as the next step for each event.
+  The next state is itself associated *)
+type automaton = (subimage * (event -> (time * state))) array
+
+type t = {
+  state : state (* The current state of the automaton. *) ;
+  time : time (* The number of cycles that were spent on this state without taking any transition. *) ;
+  delta : automaton (* How this state responds to events. *)
+}
+
+let image t =
+  let (image, _next) = t.delta.(t.state) in
+  image
+
+let next t e =
+  let (_image, next) = t.delta.(t.state) in
+  let (time, st) = next e in
+  if Time.(t.time < time) then
+    { t with time = Time.incr t.time }
+  else { t with state = st ; time = Time.zero }
+
+type sequence = (subimage * float) list
+
+let loop s =
+  let len = List.length s in
+  assert (len > 0) ;
+  let automaton =
+    Array.of_list (List.mapi (fun index (image, time) ->
+      (image, fun _ -> (Time.of_seconds time, (index + 1) mod len))) s) in {
+    delta = automaton ;
+    time = Time.zero ;
+    state = 0
+  }
+
+let static i = loop [(i, 1.)]
+
+let react t es s =
+  let len = List.length s in
+  if len = 0 then t
+  else
+    let es = ESet.of_list es in
+    let shift = Array.length t.delta in
+    let sequence =
+      Array.of_list (List.mapi (fun index (image, time) ->
+        let next =
+          if index = len - 1 then t.state
+          else shift + index + 1 in
+        (image, fun e ->
+          if ESet.mem e es then (Time.zero, shift)
+          else (Time.of_seconds time, next))) s) in
+    let automaton =
+      Array.append
+        (Array.map (fun (image, next) ->
+          (image, fun e ->
+            if ESet.mem e es then (Time.zero, shift)
+            else next e)) t.delta)
+        sequence in {
+      delta = automaton ;
+      time = Time.zero ;
+      state = 0
+    }
+
+
--- src/animation.mli
+++ src/animation.mli
@@ -10,3 +10,48 @@ type subimage
   store the corresponding subimage. *)
 val make_subimage : int -> int -> (int * int) -> subimage
 
+
+(* A representation of an object as an automaton.
+  It can respond to events to update its internal image. *)
+type t
+
+(* The events from which the automaton responds. *)
+type event =
+  (* The object is moving. *)
+  | MoveLeft
+  | MoveRight
+  (* Some user actions. *)
+  | LookDown
+  | LookUp
+  | LookBehind
+  (* The object was touched by another moving object. *)
+  | Touch
+  (* The wind reached the object (occurs randomly and propagates from left to right). *)
+  | Wind
+  (* Randomly occurring event pulsing at different frequencies. *)
+  | RandomFlicker (* Several times per second. *)
+  | RandomFrequent (* About every second. *)
+  | RandomNormal (* Every 5–10 seconds. *)
+  | RandomRare (* More than 30 seconds. *)
+  (* Time simply passes (it's a no-event). *)
+  | Tau
+
+(* The current image of the object. *)
+val image : t -> subimage
+
+(* Respond to an event. *)
+val next : t -> event -> t
+
+(* An animation sequence, composed of subimages associated with a time (in seconds). *)
+type sequence = (subimage * float) list
+
+(* An object with a single image, never changing. *)
+val static : subimage -> t
+
+(* An object doing a single looping sequence. *)
+val loop : sequence -> t
+
+(* Like its first argument except when one of the provided event is fired:
+  it then plays the sequence then goes back to its initial state. *)
+val react : t -> event list -> sequence -> t
+
