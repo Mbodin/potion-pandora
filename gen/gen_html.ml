533089384a2031f9fd21bdbd75861c66cbb655d1
Martin Bodin <martin.bodin@inria.fr>
2025-04-04 11:00:39 +0200

    Ajout de configurations pour le déploiment sur Github.

--- gen/gen_html.ml
+++ gen/gen_html.ml
@@ -10,31 +10,32 @@ let js =
     ""
   ]
 
-let html =
-  [%html {|
-    <html>
-      <head>
-        <title>Potion Pandora</title>
-        <style>
-          body {
-            background-color: #201127 ;
-          }
 
-          canvas {
-            image-rendering: pixelated ;
-            object-fit: contain ;
-          }
-        </style>
-      </head>
-      <body>
-        <noscript>
-          Cette page nécessite JavaScript pour fonctionner.
-          Son code source est disponible <a href = "https://github.com/Mbodin/potion-pandora/">ici</a>.
-        </noscript>
-        <script>|} (Html.txt js) {|</script>
-      </body>
-    </html>
-  |}]
+let%html html = {|
+  <html>
+    <head>
+      <meta charset = "utf-8" />
+      <title>Potion Pandora</title>
+      <style>
+body {
+  background-color: #201127 ;
+}
+
+canvas {
+  image-rendering: pixelated ;
+  object-fit: contain ;
+}
+      </style>
+    </head>
+    <body>
+      <noscript>
+        Cette page nécessite JavaScript pour fonctionner.
+        Son code source est disponible <a href = "https://github.com/Mbodin/potion-pandora/">ici</a>.
+      </noscript>
+      <script>|} (Html.Unsafe.data js) {|</script>
+    </body>
+  </html>
+|}
 
 let () =
   Format.printf "%a@." (Html.pp ~indent:false ()) html
